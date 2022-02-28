3d6c683ae06b4c65cd95325d1b869941892da4ac
Luka <lenixon@ucdavis.edu>
2022-02-27 20:06:45 -0800

    Removed unused files

--- UserDir/ExampleProj1/make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ ../../Source/CPPSrc/FlowVectors.cpp -o ../../Source/CPPSrc/FlowVectors -L/usr/local/include -lzmq -Wno-nullability-completeness
--- UserDir/ExampleProj1/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ROOT="../.."
-EXEC="$ROOT/Source/PySrc/slowmotion.py"
-#ENV_PATH="/Users/lukanixon/Desktop/ECS193/Super-Slow-Motion-Video/Pipfile"
-ENV_PATH="$PWD/$ROOT/Pipfile"
-export PIPENV_PIPFILE=$ENV_PATH
-pipenv run python3 $EXEC --input ./akiyo_cif.y4m --output ./Outputs/akiyo_cif.mkv --FPS 25 --slowdown 4
