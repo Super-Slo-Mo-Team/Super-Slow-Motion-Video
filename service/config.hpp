8ca18dde40345553067399c6e72e0c08653abd56
MaxMol1 <mmolchanov@ucdavis.edu>
2022-05-18 10:27:07 -0700

    Update config paths

--- service/config.hpp
+++ service/config.hpp
@@ -8,12 +8,11 @@
 #define ROOT_PATH "tmp"
 #define YUV_PATH "tmp/yuv"
 #define FLO_PATH "tmp/flo"
-#define R_FRAME_PATH "tmp/rFrames"
-#define I_FRAME_PATH "tmp/iFrames"
 
 // model checkpoints location
+#define MODEL_SCRIPT "../model/model.py"
 #define FRAME_INTERPOLATION_MODEL_PATH "../model/checkpoints/FrameInterpolation.ckpt"
-#define BACKWARP_MODEL_PATH "../model/checkpoints/BackWarp.ckpt"
+#define BACKWARP_MODEL_PATH "../model/checkpoints/traced_backwarp_model.pt"
 
 // IPC comm
 #define FV_SOCKET_PATH "ipc://tmp/flowVec.sock"
