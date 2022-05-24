50da056db2e0ad770d17d290555c59635951d9b3
MaxMol1 <mmolchanov@ucdavis.edu>
2022-05-23 23:09:16 -0700

    Add to test script

--- example_script.sh
+++ example_script.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+INPUT="IO/input.y4m"
+OUTPUT="IO/output.y4m"
+SLOWDOWN=2
+FPS=50
+
+TMP="IO/temp.yuv"
+
+rm build/yuv/*
+
 cd build
-./Super_Slow_Motion_Video ../IO/input.y4m ../IO/output.y4m 4 30
+./Super_Slow_Motion_Video "../$INPUT" "../$OUTPUT" $SLOWDOWN $FPS
 cd ..
 
-cat build/tmp/yuv/*.yuv > IO/temp.yuv
-ffmpeg -f rawvideo -pix_fmt yuv420p -s:v 352x288 -r 30 -i IO/temp.yuv -c:v libx264 IO/res.mp4
+cat build/tmp/yuv/*.yuv > $TMP
+ffmpeg -f rawvideo -pix_fmt yuv420p -s:v 352x288 -r $FPS -i $TMP -c:v libx264 $OUTPUT
 rm IO/temp.yuv
