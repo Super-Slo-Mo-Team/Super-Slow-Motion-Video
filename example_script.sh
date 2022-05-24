cd1ec5454c98742ee19bc470b7fa3fdc53106b55
MaxMol1 <mmolchanov@ucdavis.edu>
2022-05-23 22:53:48 -0700

    Add video reconstruction command temporarily

--- example_script.sh
+++ example_script.sh
@@ -3,3 +3,7 @@
 cd build
 ./Super_Slow_Motion_Video ../IO/input.y4m ../IO/output.y4m 4 30
 cd ..
+
+cat build/tmp/yuv/*.yuv > IO/temp.yuv
+ffmpeg -f rawvideo -pix_fmt yuv420p -s:v 352x288 -r 30 -i IO/temp.yuv -c:v libx264 IO/res.mp4
+rm IO/temp.yuv
